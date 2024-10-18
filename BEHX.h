4470b967ff586b21a17578802269ce37d2d41e69
Félix <142559040+kapitche@users.noreply.github.com>
2024-10-18 13:04:46 -0400

    Update BEHX.h

--- BEHX.h
+++ BEHX.h
@@ -19,20 +19,23 @@
 #include <typeinfo>
 #include <string>
 
-template <typename T, typename... Args> void print(T elem, Args... args) 
+template <typename T, typename... Args> 
+void print(T elem, Args... args) 
 {
     std::cout << elem;
     (std::cout << ... << args);
 }
 
-template <typename T> T input() 
+template <typename T> 
+T input() 
 {
     T value;
     std::cin >> value;
     return value;
 }
 
-template <typename T> T input(const std::string& prompt) 
+template <typename T> 
+T input(const std::string& prompt) 
 {
     T value;
     std::cout << prompt;
@@ -40,38 +43,45 @@ template <typename T> T input(const std::string& prompt)
     return value;
 }
 
-template <typename T> long long findMinimumSize() 
+template <typename T> 
+long long findMinimumSize() 
 {
     return std::numeric_limits<T>::min();
 }
 
-template <typename T> long long findMaximumSize() 
+template <typename T> 
+long long findMaximumSize() 
 {
     return std::numeric_limits<T>::max();
 }
 
-template <typename T> void printTypeInfo() 
+template <typename T> 
+void printTypeInfo() 
 {
     std::cout << "Type: " << typeid(T).name() << "\n";
     std::cout << "Size: " << sizeof(T) << " bytes";
 }
 
-template <typename T> T add(T a, T b) 
+template <typename T> 
+T add(T a, T b) 
 {
     return a + b;
 }
 
-template <typename T> T subtract(T a, T b) 
+template <typename T> 
+T subtract(T a, T b) 
 {
     return a - b;
 }
 
-template <typename T> T multiply(T a, T b) 
+template <typename T> 
+T multiply(T a, T b) 
 {
     return a * b;
 }
 
-template <typename T> T divide(T a, T b) 
+template <typename T> 
+T divide(T a, T b) 
 {
     if (b == 0) 
     {
